4c0c5f93a8419dff21f16e64356b85b67835bc28
Daniel DeKerlegand <dldekerl@gmail.com>
2017-02-25 15:45:01 -0600

    still cant get oracle to connect

--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var categoryTree = require('./categoryTree.js');
 
 /* GET users listing. */
-router.get('/category', function(req, res, next) {
-  console.log(req);
-  res.send('respond with a resource');
+router.get('/', function(req, res, next) {
+  var category = req.query.category;
+  categoryTree(category);
+  //res.render('categorytree', { categorytree: data });
 });
 
 module.exports = router;
