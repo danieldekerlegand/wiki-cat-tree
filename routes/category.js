61281fab5bb455f8c90312df745aaec0f805091b
Daniel DeKerlegand <dldekerl@gmail.com>
2017-02-25 16:05:38 -0600

    try node-oracle

--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var categoryTree = require('./categoryTree.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   var category = req.query.category;
-  categoryTree(category);
+  //categoryTree(category);
   //res.render('categorytree', { categorytree: data });
 });
 
